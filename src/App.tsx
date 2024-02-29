e4bb524d9b34837e4cb681899240e550491ccea8
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-29 22:33:48 +0530

    Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
+import Button from './components/Button'; //Button is aa functional component
 
 function App() {
   return (
     <div className="App">
       <h1>TypeScript with react</h1>
-
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+//making button components in tsx
+import React from 'react'
+
+//React.FC is a functional component
+const Button: React.FC = () => {
+    return <button></button>
+}
+
+export default Button;
