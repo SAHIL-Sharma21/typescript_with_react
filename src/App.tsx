18ed21ece0f82a4de6c6555b089c839c1e9062e7
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-03-01 21:58:50 +0530

    props passing in react with TS by using interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Button from './components/Button'; //Button is aa functional component
 
+
 function App() {
   return (
     <div className="App">
       <h1>TypeScript with react</h1>
-      <Button />
+      <Button text='Click btn' />
+      <Button text='another way'/>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,22 @@
 //making button components in tsx
 import React from 'react'
 
+//preffered way of passing the props we can use interface or create type
+interface MyButton {
+    text: string,
+}
+
 //React.FC is a functional component
-const Button: React.FC = () => {
-    return <button></button>
+// giving generics calss to our Button: React.FC
+//one way to write code
+// const Button: React.FC<MyButton> = (props) => {
+//     return <button>{props.text}</button>
+// }
+
+
+//another way we can do like this
+const Button: React.FC<MyButton> = ({text}) => {
+    return <button>{text}</button>
 }
 
 export default Button;
